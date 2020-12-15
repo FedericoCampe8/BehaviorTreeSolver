83d6ee4337327ce064e4a695506756f702141ea8
Void <ftardivo@nmsu.edu>
2020-12-14 22:09:48 -0700

    Update benchmark

--- runBenchmarks.py
+++ runBenchmarks.py
@@ -28,30 +28,32 @@ while mainQueueMaxSize <= mainQueueMaxSizeMax:
     mainQueueMaxSizes.append(mainQueueMaxSize)
     mainQueueMaxSize = mainQueueMaxSize * 2
 
+gpuParallelisms = [1000,2000,4000]
+
 binPath = str(sys.argv[1])
 inFilePath = str(sys.argv[2])
 timeoutSeconds = str(sys.argv[3])
 mddWidth = str(sys.argv[4])
-gpuMaxParallelism = str(sys.argv[5])
 
 inFileName = os.path.basename(inFilePath).split('.')[0]
-outFileName = inFileName + "T" + timeoutSeconds + "W" + mddWidth + "P" + gpuMaxParallelism + ".csv"
+outFileName = inFileName + "T" + timeoutSeconds + "W" + mddWidth + ".csv"
 outFile = open(outFileName, "w")
 
 for mainQueueMaxSize in mainQueueMaxSizes:
     for mainQueueReducedSize in mainQueueReducedSizes:
-        cmd = [binPath] + [inFilePath] + [timeoutSeconds] + [str(mainQueueMaxSize)] + [str(mainQueueReducedSize)] + [mddWidth] + [gpuMaxParallelism]
-        print("Running " + str(cmd) + " -> ", end="" )
-        sys.stdout.flush()
-        output = "[RESULT] Solution: [] | Value: 0 | Time: 0 ms (00h00m00s) | Iterations: 0 | Visited states: 0"
-        try:
-            tmpOutput = subprocess.check_output(cmd, stderr=subprocess.STDOUT, encoding="ascii")
-            output = tmpOutput.split("\n")[-2]
-        except subprocess.CalledProcessError:
-           pass
-        value = getSolutionValue(output)
-        print(value)
-        info = value + "," + str(mainQueueMaxSize) + "," + str(mainQueueReducedSize)
-        outFile.write(info + "\n")
+        for gpuParallelism in gpuParallelisms:
+            cmd = [binPath] + [inFilePath] + [timeoutSeconds] + [str(mainQueueMaxSize)] + [str(mainQueueReducedSize)] + [mddWidth] + [str(gpuParallelism)]
+            print("Running " + str(cmd) + " -> ", end="" )
+            sys.stdout.flush()
+            output = "[RESULT] Solution: [] | Value: 0 | Time: 0 ms (00h00m00s) | Iterations: 0 | Visited states: 0"
+            try:
+                tmpOutput = subprocess.check_output(cmd, stderr=subprocess.STDOUT, encoding="ascii")
+                output = tmpOutput.split("\n")[-2]
+            except subprocess.CalledProcessError:
+               pass
+            value = getSolutionValue(output)
+            print(value)
+            info = value + "," + str(mainQueueMaxSize) + "," + str(mainQueueReducedSize) + "," + str(gpuParallelism)
+            outFile.write(info + "\n")
 
 outFile.close()
