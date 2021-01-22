7d6c13792d438864943a91d5c6c83188afda1195
Void <ftardivo@nmsu.edu>
2021-01-22 09:10:22 -0700

    Hybrid

--- src/DD/MDD.cuh
+++ src/DD/MDD.cuh
@@ -23,10 +23,12 @@ namespace DD
         unsigned int const width;
         ProblemType const * const problem;
         private:
+        StateType top;
+        StateType bottom;
+        LightVector<StateType>* currentStates;
+        LightVector<StateType>* nextStates;
         Vector<StateType> cutset;
-        Vector<StateType> currentStates;
-        Vector<StateType> nextStates;
-        Vector<AuxiliaryData> auxiliaryData;
+        LightVector<AuxiliaryData>* auxiliaryData;
 
         // Functions
         public:
@@ -48,6 +50,8 @@ namespace DD
         __host__ __device__ void shirkToFitAuxiliaryData();
         __host__ __device__ inline void sortAuxiliaryData();
         __host__ __device__ void swapCurrentAndNextStates();
+        __host__ __device__ void initTop();
+        __host__ __device__ void saveBottom();
 
     };
 }
@@ -56,10 +60,9 @@ template<typename ProblemType, typename StateType>
 DD::MDD<ProblemType, StateType>::MDD(ProblemType const * problem, unsigned int width, Memory::MallocType mallocType) :
     width(width),
     problem(problem),
-    cutset(width * problem->maxBranchingFactor, mallocType),
-    currentStates(width, mallocType),
-    nextStates(width, mallocType),
-    auxiliaryData(width * problem->maxBranchingFactor, mallocType)
+    top(problem, mallocType),
+    bottom(problem, mallocType),
+    cutset(width * problem->maxBranchingFactor, mallocType)
 {
     // Cutset states
     LightArray<StateType> cutsetArray = cutset;
@@ -70,24 +73,6 @@ DD::MDD<ProblemType, StateType>::MDD(ProblemType const * problem, unsigned int w
         new (cutsetArray[cutsetStateIdx]) StateType(problem, storages);
         storages += storageSize;
     }
-
-    // Current states
-    LightArray<StateType> currentStatesArray = currentStates;
-    storages = StateType::getStorages(problem, cutsetArray.getCapacity(), mallocType);
-    for (unsigned int currentStateIdx = 0; currentStateIdx < currentStatesArray.getCapacity(); currentStateIdx += 1)
-    {
-        new (currentStatesArray[currentStateIdx])  StateType(problem, storages);
-        storages += storageSize;
-    }
-
-    // Next states
-    LightArray<StateType> nextStatesArray = nextStates;
-    storages = StateType::getStorages(problem, cutsetArray.getCapacity(), mallocType);
-    for (unsigned int nextStateIdx = 0; nextStateIdx < nextStatesArray.getCapacity(); nextStateIdx += 1)
-    {
-        new (nextStatesArray[nextStateIdx])  StateType(problem, storages);
-        storages += storageSize;
-    }
 }
 
 
@@ -95,10 +80,58 @@ template<typename ProblemType, typename StateType>
 __host__ __device__
 void DD::MDD<ProblemType, StateType>::buildTopDown(DD::Type type, Neighbourhood const * neighbourhood)
 {
+    std::byte* freeScratchpadMem;
+#ifdef __CUDA_ARCH__
+    __shared__ uint32_t scratchpadMem[1000];
+    freeScratchpadMem = reinterpret_cast<std::byte*>(&scratchpadMem);
+#else
+    freeScratchpadMem = Memory::safeMalloc(sizeof(uint32_t) * 100000, Memory::MallocType::Std);
+#endif
+#ifdef __CUDA_ARCH__
+    if(threadIdx.x == 0)
+#endif
+    {
+        // Current states
+        currentStates = reinterpret_cast<LightVector<StateType>*>(freeScratchpadMem);
+        freeScratchpadMem += sizeof(LightVector<StateType>);
+        new(currentStates) LightVector<StateType>(width, reinterpret_cast<StateType*>(freeScratchpadMem));
+        freeScratchpadMem = Memory::align(8, currentStates->endOfStorage());
+        unsigned int const storageSize = StateType::sizeOfStorage(problem);
+        for (unsigned int stateIdx = 0; stateIdx < currentStates->getCapacity(); stateIdx += 1)
+        {
+            new(currentStates->LightArray<StateType>::at(stateIdx)) StateType(problem, freeScratchpadMem);
+            freeScratchpadMem += storageSize;
+        }
+        freeScratchpadMem = Memory::align(8, freeScratchpadMem);
+
+        // Next states
+        nextStates = reinterpret_cast<LightVector<StateType>*>(freeScratchpadMem);
+        freeScratchpadMem += sizeof(LightVector<StateType>);
+        new(nextStates) LightVector<StateType>(width, reinterpret_cast<StateType*>(freeScratchpadMem));
+        freeScratchpadMem = Memory::align(8, nextStates->endOfStorage());
+        for (unsigned int stateIdx = 0; stateIdx < nextStates->getCapacity(); stateIdx += 1)
+        {
+            new (nextStates->LightArray<StateType>::at(stateIdx)) StateType(problem, freeScratchpadMem);
+            freeScratchpadMem += storageSize;
+        }
+        freeScratchpadMem = Memory::align(8, freeScratchpadMem);
+
+        // Auxiliary information
+        auxiliaryData = reinterpret_cast<LightVector<AuxiliaryData>*>(freeScratchpadMem);
+        freeScratchpadMem += sizeof(LightVector<AuxiliaryData>);
+        new(auxiliaryData) LightVector<AuxiliaryData>(width * problem->maxBranchingFactor, reinterpret_cast<AuxiliaryData*>(freeScratchpadMem));
+
+        initTop();
+    }
+
+#ifdef __CUDA_ARCH__
+    __syncthreads();
+#endif
+
     // Build
     bool cutsetSaved = false;
     unsigned int const variablesCount = problem->variables.getCapacity();
-    for (unsigned int variableIdx = currentStates[0]->selectedValues.getSize(); variableIdx < variablesCount; variableIdx += 1)
+    for (unsigned int variableIdx = currentStates->at(0)->selectedValues.getSize(); variableIdx < variablesCount; variableIdx += 1)
     {
         resetAuxiliaryData();
         calcAuxiliaryData(variableIdx, neighbourhood);
@@ -108,7 +141,7 @@ void DD::MDD<ProblemType, StateType>::buildTopDown(DD::Type type, Neighbourhood 
 #ifdef __CUDA_ARCH__
         __syncthreads();
 #endif
-        if (nextStates.isEmpty())
+        if (nextStates->isEmpty())
         {
             setInvalid();
             return;
@@ -128,6 +161,8 @@ void DD::MDD<ProblemType, StateType>::buildTopDown(DD::Type type, Neighbourhood 
         __syncthreads();
 #endif
     }
+
+    saveBottom();
 }
 
 template<typename ProblemType, typename StateType>
@@ -141,18 +176,15 @@ template<typename ProblemType, typename StateType>
 __host__ __device__
 StateType* DD::MDD<ProblemType, StateType>::getBottom() const
 {
-    return currentStates[0];
+    return const_cast<StateType*>(&bottom);
 }
 
 template<typename ProblemType, typename StateType>
 __host__ __device__
 void DD::MDD<ProblemType, StateType>::setTop(StateType const * state)
 {
-    currentStates.clear();
-    nextStates.clear();
     cutset.clear();
-
-    currentStates.pushBack(state);
+    top = *state;
 }
 
 
@@ -164,7 +196,7 @@ void DD::MDD<ProblemType, StateType>::setInvalid()
     if(threadIdx.x == 0)
 #endif
     {
-        getBottom()->setInvalid();
+        bottom.setInvalid();
     }
 }
 
@@ -176,12 +208,12 @@ void DD::MDD<ProblemType, StateType>::calcAuxiliaryData(unsigned int variableIdx
 #ifdef __CUDA_ARCH__
     __syncthreads();
     unsigned int const currentStateIdx = threadIdx.x / problem->maxBranchingFactor;
-    if (currentStateIdx < currentStates.getSize())
+    if (currentStateIdx < currentStates->getSize())
 #else
-    for(unsigned int currentStateIdx = 0; currentStateIdx < currentStates.getSize(); currentStateIdx += 1)
+    for(unsigned int currentStateIdx = 0; currentStateIdx < currentStates->getSize(); currentStateIdx += 1)
 #endif
     {
-        StateType const * const currentState = currentStates[currentStateIdx];
+        StateType const * const currentState = currentStates->at(currentStateIdx);
 #ifdef __CUDA_ARCH__
         unsigned int const admissibleValueIdx = threadIdx.x % problem->maxBranchingFactor;
         if(admissibleValueIdx < currentState->admissibleValues.getSize())
@@ -196,7 +228,7 @@ void DD::MDD<ProblemType, StateType>::calcAuxiliaryData(unsigned int variableIdx
             {
                 unsigned int const valueIdx = value - problem->variables[variableIdx]->minValue;
                 unsigned int const auxiliaryDataIdx = problem->maxBranchingFactor * currentStateIdx + valueIdx;
-                auxiliaryData[auxiliaryDataIdx]->cost = calcCost(problem, currentState, value);
+                auxiliaryData->at(auxiliaryDataIdx)->cost = calcCost(problem, currentState, value);
             }
         }
     }
@@ -209,16 +241,16 @@ void DD::MDD<ProblemType, StateType>::calcNextStates(unsigned int variableIdx)
 #ifdef __CUDA_ARCH__
     __syncthreads();
     unsigned int const nextStateIdx = threadIdx.x;
-    if(nextStateIdx < nextStates.getSize())
+    if(nextStateIdx < nextStates->getSize())
 #else
-    for(unsigned int nextStateIdx = 0; nextStateIdx < nextStates.getSize(); nextStateIdx += 1)
+    for(unsigned int nextStateIdx = 0; nextStateIdx < nextStates->getSize(); nextStateIdx += 1)
 #endif
     {
-        AuxiliaryData ad = *auxiliaryData[nextStateIdx];
+        AuxiliaryData ad = *auxiliaryData->at(nextStateIdx);
         unsigned int const currentStateIdx = ad.index / problem->maxBranchingFactor ;
         unsigned int const valueIdx = ad.index % problem->maxBranchingFactor ;
         OP::ValueType const value = problem->variables[variableIdx]->minValue + valueIdx;
-        makeState(problem, currentStates[currentStateIdx], value, auxiliaryData[nextStateIdx]->cost, nextStates[nextStateIdx]);
+        makeState(problem, currentStates->at(currentStateIdx), value, auxiliaryData->at(nextStateIdx)->cost, nextStates->at(nextStateIdx));
     }
 }
 
@@ -230,13 +262,13 @@ void DD::MDD<ProblemType, StateType>::mergeNextStates(unsigned int variableIdx)
     if(threadIdx.x == 0)
 #endif
     {
-        for (unsigned int auxiliaryDataIdx = nextStates.getSize(); auxiliaryDataIdx < auxiliaryData.getSize(); auxiliaryDataIdx += 1)
+        for (unsigned int auxiliaryDataIdx = nextStates->getSize(); auxiliaryDataIdx < auxiliaryData->getSize(); auxiliaryDataIdx += 1)
         {
-            AuxiliaryData ad = *auxiliaryData[auxiliaryDataIdx];
+            AuxiliaryData ad = *auxiliaryData->at(auxiliaryDataIdx);
             unsigned int const currentStateIdx = ad.index / problem->maxBranchingFactor;
             unsigned int const valueIdx = ad.index % problem->maxBranchingFactor;
             OP::ValueType const value = problem->variables[variableIdx]->minValue + valueIdx;
-            mergeState(problem, currentStates[currentStateIdx], value, auxiliaryData[auxiliaryDataIdx]->cost, nextStates.back());
+            mergeState(problem, currentStates->at(currentStateIdx), value, auxiliaryData->at(auxiliaryDataIdx)->cost, nextStates->back());
         }
     }
 }
@@ -245,17 +277,17 @@ template<typename ProblemType, typename StateType>
 __host__ __device__
 void DD::MDD<ProblemType, StateType>::resetAuxiliaryData()
 {
-    auxiliaryData.resize(currentStates.getSize() * problem->maxBranchingFactor); //resizeAuxiliaryData();
+    auxiliaryData->resize(currentStates->getSize() * problem->maxBranchingFactor); //resizeAuxiliaryData();
 #ifdef __CUDA_ARCH__
     __syncthreads();
     unsigned int const auxiliaryDataIdx = threadIdx.x;
-    if(auxiliaryDataIdx < auxiliaryData.getSize())
+    if(auxiliaryDataIdx < auxiliaryData->getSize())
 #else
-    for(unsigned int auxiliaryDataIdx = 0; auxiliaryDataIdx < auxiliaryData.getSize(); auxiliaryDataIdx += 1)
+    for(unsigned int auxiliaryDataIdx = 0; auxiliaryDataIdx < auxiliaryData->getSize(); auxiliaryDataIdx += 1)
 #endif
     {
-        auxiliaryData[auxiliaryDataIdx]->cost = DP::MaxCost;
-        auxiliaryData[auxiliaryDataIdx]->index = auxiliaryDataIdx;
+        auxiliaryData->at(auxiliaryDataIdx)->cost = DP::MaxCost;
+        auxiliaryData->at(auxiliaryDataIdx)->index = auxiliaryDataIdx;
     }
 }
 
@@ -267,7 +299,7 @@ void DD::MDD<ProblemType, StateType>::resizeAuxiliaryData()
     if(threadIdx.x == 0)
 #endif
     {
-        auxiliaryData.resize(currentStates.getSize() * problem->maxBranchingFactor);
+        auxiliaryData->resize(currentStates->getSize() * problem->maxBranchingFactor);
     }
 }
 
@@ -279,7 +311,7 @@ void DD::MDD<ProblemType, StateType>::resizeCutset()
     if(threadIdx.x == 0)
 #endif
     {
-        cutset.resize(nextStates.getSize());
+        cutset.resize(nextStates->getSize());
     }
 }
 
@@ -296,7 +328,7 @@ void DD::MDD<ProblemType, StateType>::resizeNextStates(unsigned int variableIdx,
         {
             nextStatesCount = 1;
         }
-        nextStates.resize(min(nextStatesCount, auxiliaryData.getSize()));
+        nextStates->resize(min(nextStatesCount, auxiliaryData->getSize()));
     }
 }
 
@@ -304,7 +336,7 @@ template<typename ProblemType, typename StateType>
 __host__ __device__
 void DD::MDD<ProblemType, StateType>::saveCutset()
 {
-    cutset.resize(nextStates.getSize()); //resizeCutset();
+    cutset.resize(nextStates->getSize()); //resizeCutset();
 #ifdef __CUDA_ARCH__
     __syncthreads();
     unsigned int const cutsetStateIdx = threadIdx.x;
@@ -313,7 +345,7 @@ void DD::MDD<ProblemType, StateType>::saveCutset()
     for(unsigned int cutsetStateIdx = 0; cutsetStateIdx < cutset.getSize(); cutsetStateIdx += 1)
 #endif
     {
-        *cutset[cutsetStateIdx] = *nextStates[cutsetStateIdx];
+        *cutset[cutsetStateIdx] = *nextStates->at(cutsetStateIdx);
     }
 }
 
@@ -327,11 +359,11 @@ void DD::MDD<ProblemType, StateType>::shirkToFitAuxiliaryData()
 #endif
     {
         AuxiliaryData const invalidAuxiliaryData(DP::MaxCost, 0);
-        AuxiliaryData const* const auxiliaryDataEnd = thrust::lower_bound(thrust::seq, auxiliaryData.begin(), auxiliaryData.end(), invalidAuxiliaryData);
-        if (auxiliaryDataEnd != auxiliaryData.end())
+        AuxiliaryData const* const auxiliaryDataEnd = thrust::lower_bound(thrust::seq, auxiliaryData->begin(), auxiliaryData->end(), invalidAuxiliaryData);
+        if (auxiliaryDataEnd != auxiliaryData->end())
         {
-            unsigned int size = auxiliaryData.indexOf(auxiliaryDataEnd);
-            auxiliaryData.resize(size);
+            unsigned int size = auxiliaryData->indexOf(auxiliaryDataEnd);
+            auxiliaryData->resize(size);
         }
     }
 }
@@ -341,9 +373,9 @@ __host__ __device__
 void DD::MDD<ProblemType, StateType>::sortAuxiliaryData()
 {
 #ifdef __CUDA_ARCH__
-    Algorithms::oddEvenSort(auxiliaryData.front(), auxiliaryData.getSize());
+    Algorithms::oddEvenSort(auxiliaryData->front(), auxiliaryData->getSize());
 #else
-    thrust::sort(thrust::seq, auxiliaryData.begin(), auxiliaryData.end());
+    thrust::sort(thrust::seq, auxiliaryData->begin(), auxiliaryData->end());
 #endif
 }
 
@@ -355,6 +387,27 @@ void DD::MDD<ProblemType, StateType>::swapCurrentAndNextStates()
     if(threadIdx.x == 0)
 #endif
     {
-        LightVector<StateType>::swap(currentStates, nextStates);
+        LightVector<StateType>::swap(*currentStates, *nextStates);
+    }
+}
+template<typename ProblemType, typename StateType>
+__host__ __device__ void DD::MDD<ProblemType, StateType>::initTop()
+{
+#ifdef __CUDA_ARCH__
+    if(threadIdx.x == 0)
+#endif
+    {
+        currentStates->resize(1);
+        *currentStates->at(0) = top;
+    }
+}
+template<typename ProblemType, typename StateType>
+__host__ __device__ void DD::MDD<ProblemType, StateType>::saveBottom()
+{
+#ifdef __CUDA_ARCH__
+    if(threadIdx.x == 0)
+#endif
+    {
+        bottom = *currentStates->at(0);
     }
 }
