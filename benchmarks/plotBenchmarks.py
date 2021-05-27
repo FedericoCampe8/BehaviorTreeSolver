cb6317c9eb9ea351130fb817fdb1496124525f0f
95A31 <ftardivo@nmsu.edu>
2021-05-27 10:17:31 -0600

    Plot and summary rework

--- benchmarks/plotBenchmarks.py
+++ benchmarks/plotBenchmarks.py
@@ -6,7 +6,6 @@ import common
 
 for benchmark in ["Ctwp", "Jsp", "Mosp", "Tsppd"]:
     print("Plotting {} ...".format(benchmark))
-
     results_dir = "./{}/{}".format(benchmark, common.Directories["results"])
     results_files = os.listdir(results_dir)
     results_files.sort()
@@ -19,21 +18,23 @@ for benchmark in ["Ctwp", "Jsp", "Mosp", "Tsppd"]:
         dataframes.append(df)
     df = pd.concat(dataframes, ignore_index=True)
     timeouts = df.Timeout.unique()
-    for timeout in timeouts:
-        df1 = df[df["Timeout"] == timeout]
-        plt.figure(figsize=(0.3*df1.shape[0], 4))
-        p = sns.barplot(x="Instance", y="Cost", hue="Solver", data=df1, ci=None)
-        p.set_xticklabels(p.get_xticklabels(), rotation=90)
-        p.set_ylabel("Cost")
-        p.set_title("Timeout {}s".format(timeout))
-        plot_file = "{}-{}-cost.pdf".format(benchmark, timeout)
-        plt.savefig(plot_file, dpi=300, bbox_inches="tight")
-        plt.clf()
-        p = sns.barplot(x="Instance", y="Time", hue="Solver", data=df1, ci=None)
-        p.set_xticklabels(p.get_xticklabels(), rotation=90)
-        p.set_ylabel("Time [s]")
-        p.set_title("Timeout {}s".format(timeout))
-        plot_file = "{}-{}-time.pdf".format(benchmark, timeout)
-        plt.savefig(plot_file, bbox_inches="tight")
-        plt.clf()
-        #plt.show()
+    h = 4
+    w = 0.3*df.shape[0]/len(timeouts)
+    g = sns.FacetGrid(df, row="Timeout", height=h, aspect=w/h, row_order=timeouts )
+    # Costs
+    g.map_dataframe(sns.barplot, x="Instance", y="Cost", hue="Solver", order=None, data=df, ci=None, palette="tab10")
+    g.set_ylabels("Cost")
+    for axes in g.axes.flat:
+        axes.set_xticklabels(axes.get_xticklabels(), rotation=90)
+    g.add_legend()
+    plot_file = "{}-cost.pdf".format(benchmark,)
+    g.savefig(plot_file, dpi=300, bbox_inches="tight")
+    # Time
+    g = sns.FacetGrid(df, row="Timeout", height=h, aspect=w/h, row_order=timeouts )
+    g.map_dataframe(sns.barplot, x="Instance", y="Time", hue="Solver", order=None, data=df, ci=None, palette="tab10")
+    g.set_ylabels("Time [s]")
+    for axes in g.axes.flat:
+        axes.set_xticklabels(axes.get_xticklabels(), rotation=90)
+    g.add_legend()
+    plot_file = "{}-time.pdf".format(benchmark,)
+    g.savefig(plot_file, dpi=300, bbox_inches="tight")
